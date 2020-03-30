8ebb6bf2bf123ad058c9b93011a293804da3109c
Ian O'Malley <iomalley@localhost.localdomain>
2020-03-30 12:43:42 -0400

    Use webpack plugin for war instead of bash script

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,24 @@
+const { WebpackWarPlugin } = require('webpack-war-plugin');
+
+const webpackWarPluginOptions = {
+    archiveName: "monster-slayer",
+    webInf: "./WEB-INF"
+}
+
+/**
+ * publicPath should be the expected endpoint name.
+ * - It needs to be prepended with a forward slash.
+ * - The archiveName should match the publicPath - without the forward slash.
+ * - The WEB-INF/jboss-web.xml should be identical to the publicPath - including the forward slash.
+ */ 
 module.exports = { 
     devServer: { 
         host: "localhost" 
     },
-    publicPath: "/monster-slayer"
+    publicPath: "/monster-slayer",
+    configureWebpack: {
+        plugins: [
+            new WebpackWarPlugin(webpackWarPluginOptions)
+        ]
+    }
 }
