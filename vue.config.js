4f27bdaf448a0258e91c49c7575f031ec2a393b5
Ian O'Malley <iomalley@localhost.localdomain>
2020-03-30 13:44:37 -0400

    Removed a misleading comment

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,6 @@ const webpackWarPluginOptions = {
 /**
  * publicPath should be the expected endpoint name.
  * - It needs to be prepended with a forward slash.
- * - The archiveName should match the publicPath - without the forward slash.
  * - The WEB-INF/jboss-web.xml should be identical to the publicPath - including the forward slash.
  */ 
 module.exports = { 
