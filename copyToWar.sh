eb4b0eaeb5053683dd4dddf892a280126abd08ba
Ian O'Malley <iomalley@localhost.localdomain>
2020-03-30 09:49:10 -0400

    Write a script to do it for you

---
+++ copyToWar.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Get the public path defined in the vue config file - war will be named the same.
+ROOT_CONTEXT="/"
+PUBLIC_PATH=$(awk '/publicPath/ {gsub("\"","",$2); print $2}' ./vue.config.js)
+WAR_NAME="$(basename "$PUBLIC_PATH")"
+
+# If public path is not set, it defaults to '/' and the war will be named after the parent directory
+if [ -z "$PUBLIC_PATH" ]; then
+    PUBLIC_PATH="$ROOT_CONTEXT"
+    WAR_NAME="$(basename "$PWD")"
+fi
+
+# Replace the root context of the jboss-web file to match that of the vue config
+sed -i -e "s|<context-root>.*<\/context-root>|<context-root>${PUBLIC_PATH}<\/context-root>|" ./WEB-INF/jboss-web.xml
+
+# Clean up old war
+rm -rf $WAR_NAME.war
+
+# Hop into the dist and package it up
+pushd ./dist
+zip -r9 ../$WAR_NAME.war ./*
+popd
+
+# Add the JBOSS WEB-INF stuff
+zip -ur9 ./$WAR_NAME.war ./WEB-INF
