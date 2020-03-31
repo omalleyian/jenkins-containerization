4d72cd032581e93070f4fd65bf1f0fb7ef25a79c
esmithdev8 <esmithdev8@gmail.com>
2020-03-31 12:25:27 -0400

    Running in parallel.

--- src/tests/appTest.js
+++ src/tests/appTest.js
@@ -1,7 +1,8 @@
 module.exports = {
     'Test Case'(browser) {
-        browser.url('http://localhost:8081')
+        browser.url('http://localhost:8080')
         .waitForElementVisible('.text-center')
         .assert.containsText(".text-center", "YOU")
+        .end();
     }
 }
