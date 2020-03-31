0499ff5d08499219c3529cb80e9c30a25618b800
esmithdev8 <esmithdev8@gmail.com>
2020-03-31 14:26:45 -0400

    Testing change.

--- src/tests/appTest.js
+++ src/tests/appTest.js
@@ -1,6 +1,6 @@
 module.exports = {
     'Test Case'(browser) {
-        browser.url('http://localhost:8080')
+        browser.url('http://34.66.129.27:8080/monster-slayer')
         .waitForElementVisible('.text-center')
         .assert.containsText(".text-center", "YOU")
         .end();
