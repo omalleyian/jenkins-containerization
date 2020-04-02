6da03ad4c7df1d26921327dfd034f6cc8af64829
DarkFathom <62442659+DarkFathom@users.noreply.github.com>
2020-04-02 08:07:44 -0400

    Update nightwatch.conf.js

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,8 +1,10 @@
 const GECKO_DRIVER = require('geckodriver');
+const IE_DRIVER = require('iedriver');
 const SELENIUM_SERVER = require('selenium-server');
 
 module.exports = {
   src_folders: ["src/tests"],
+  page_objects_path: ["page-objects"],
   output_folder: "output/reports",
   parallel_process_delay: 10,
   "test_workers": {
@@ -16,13 +18,14 @@ module.exports = {
     server_path: SELENIUM_SERVER.path,
     port: 4444,
     cli_args: {
-      "webdriver.gecko.driver": GECKO_DRIVER.path
+      "webdriver.gecko.driver": GECKO_DRIVER.path,
+      "webdriver.ie.driver": IE_DRIVER.path
     }
   },
 
   test_settings: {
     default: {
-      launch_url: "http://localhost",
+      launch_url: "35.223.154.181:8080/monster-slayer/",
       selenium_port: 4444,
       selenium_host: "localhost",
       desiredCapabilities: {
@@ -38,6 +41,12 @@ module.exports = {
         "moz:firefoxOptions": {
             args: ['--headless']
         }
+      }
+    },
+    ie: {
+      desiredCapabilities: {
+        browserName: 'internet explorer',
+        javascriptEnabled: true
       }
     }
   }
